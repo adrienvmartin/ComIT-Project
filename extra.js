57ba984940562660e1188ecca99245997a4d759a
Adrien Martin <adrien.v.martin@gmail.com>
2018-08-12 11:28:22 -0600

    got form submission working, added extra functions, preparing to add mongo

---
+++ extra.js
@@ -0,0 +1,87 @@
+// Extra functions for the app
+
+function numberToDate(date) {
+
+        let string = date.toString();
+
+        let year = string.slice(0, 4);
+        let month = string.slice(5, 7);
+        let day = string.slice(8, 10);
+
+        let writtenMonth;
+        let writtenDay;
+
+        if (month == 01) {
+            writtenMonth = "January";
+        }
+
+        else if (month == 02) {
+            writtenMonth = "February";
+        }
+
+        else if (month == 03) {
+            writtenMonth = "March";
+        }
+
+        else if (month == 04) {
+            writtenMonth = "April";
+        }
+
+        else if (month == 05) {
+            writtenMonth = "May";
+        }
+
+        else if (month == 06) {
+            writtenMonth = "June";
+        }
+
+        else if (month == 07) {
+            writtenMonth = "July";
+        }
+
+        else if (month == 08) {
+            writtenMonth = "August";
+        }
+
+        else if (month == 09) {
+            writtenMonth = "September";
+        }
+
+        else if (month == 10) {
+            writtenMonth = "October";
+        }
+
+        else if (month == 11) {
+            writtenMonth = "November";
+        }
+
+        else if (month == 12) {
+            writtenMonth = "December";
+        }
+
+        if (day.charAt(0) == 1) {
+            writtenDay = `${day}th`;
+        }
+
+        else if (day.charAt(1) == 1) {
+            writtenDay = `${day}st`;
+        }
+
+        else if (day.charAt(1) == 2) {
+            writtenDay = `${day}nd`;
+        }
+
+        else if (day.charAt(1) == 3) {
+            writtenDay = `${day}rd`;
+        }
+
+        else {
+            writtenDay = `${day}th`;
+        }
+
+        let finalDate = `${writtenMonth} ${writtenDay}, ${year}`;
+        return finalDate;
+
+    }
+
+    let writtenDate = numberToDate(date);
--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Concert Database', message: 'Concert database' });
+  res.render('layout', { title: 'Concert Database', message: 'Concert database' });
 });
 
 app.get('/newshow', (req, res) => {
-    res.render('newshow', { title: 'Add Concert', message: 'Add New Headliner Show'});
+    res.render('newshow', { title: 'Concert Database', message: 'Add New Show'});
 });
 
 app.get('/concert', (req, res) => {
@@ -16,13 +16,13 @@ app.get('/concert', (req, res) => {
     const city = req.query.city;
     const venue = req.query.venue;
     const date = req.query.date;
-    // const openers = req.query.newForm;
+    const openers = req.query.openers;
 
     // After receiving the query from the first page, generate a new page with the appropriate number of fields for each band
 
     // use res.render, with the variables being used for the number of fields
     
-    res.send(`We got the following values from the query string: ${headliner}, ${date} at ${venue}, ${city}`);
+    res.send(`We got the following values from the query string: ${headliner} w/ ${openers} - ${date} @ ${venue}, ${city}`);
     
   });
 
