f4653f1a51b8e1eb6f4d9a9a819637a5d5c05a26
Adrien Martin <adrien.v.martin@gmail.com>
2018-08-12 12:01:53 -0600

    Added MongoDB to the project

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 const express = require('express');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.render('layout', { title: 'Concert Database', message: 'Concert database' });
-});
+}); */
 
 app.get('/newshow', (req, res) => {
-    res.render('newshow', { title: 'Concert Database', message: 'Add New Show'});
+    res.render('newshow', { title: 'Concert Database', message: 'Add New Show' });
 });
 
-app.get('/concert', (req, res) => {
-    const headliner = req.query.headliner;
-    const city = req.query.city;
-    const venue = req.query.venue;
-    const date = req.query.date;
-    const openers = req.query.openers;
+app.get('/', (req, res) => {
+    MongoClient.connect(url, function (err, client) {
+        const db = client.db('showtest');
+        const collection = db.collection('show1');
+
+        collection.find({}).toArray((error, documents) => {
+            client.close();
+            res.render('layout', { documents: documents });
+        });
+    });
+});
+
+
+// Saving this in case I need it later
+
+app.get('/showsubmit', (req, res) => {
+    MongoClient.connect(url, function (err, client) {
+
+        const db = client.db('showtest');
+        const collection = db.collection('show1');
+
+        const newshow = { "headliner": req.query.headliner, "openers": req.query.openers, "city": req.query.city, "venue": req.query.venue, "date": req.query.date };
+
+        collection.insertOne(newshow, (err, result) => {
+            callback(result);
+        });
+        
+        let results = collection.find().value;
+
+        let headliner = collection.find().headliner;
+        let opener = collection.find().openers;
+
+        res.send(`We got the following values from the query string: ${newshow.headliner} w/ ${newshow.opener}`);
+
+    });
 
     // After receiving the query from the first page, generate a new page with the appropriate number of fields for each band
 
     // use res.render, with the variables being used for the number of fields
-    
-    res.send(`We got the following values from the query string: ${headliner} w/ ${openers} - ${date} @ ${venue}, ${city}`);
-    
-  });
 
-app.get('/shows', (req, res) => {
-    const headliner = req.query.headliner;
-    const opener = req.query.opener;
-    const city = req.query.city;
-    const venue = req.query.venue;
-    const date = req.query.date;
 
 
 });
